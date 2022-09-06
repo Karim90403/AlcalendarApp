f75d2d68af75e4696ebd1a24362e489ebb55eec6
Karim90403 <karim90403@gmail.com>
2022-09-06 19:47:42 +0300

    add gitignore

--- api.js
+++ api.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express.Router();
 const jwt = require('jsonwebtoken');
-const dataSchema = require('./assets/schemas/dataSchema')
+const User = require('./schemas/dataSchema')
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
 const mongoose = require('mongoose');
 
 const connection = mongoose.connect("mongodb+srv://admin:!EotU322@cluster0.kcwdz.mongodb.net/calendar").then(err => {
@@ -14,27 +13,41 @@ const connection = mongoose.connect("mongodb+srv://admin:!EotU322@cluster0.kcwdz
     }
 })
 
-app.post("/registration", async (req, res) => {
-    connection
-    try {
-        let data = res.status(200).json({ login: req.body.login , password: req.body.password })
-        bcrypt.hash(data.req.body.password, saltRounds, function(err, hash) {
-            // Store hash in your password DB.
-        });
-    } catch (error) {
-        console.log(error);
-    }
+function generateAccessToken(username) {
+    return jwt.sign(username, process.env.TOKEN_SECRET);
+  }
 
+app.post("/registration", async (req, res) => {
+    if (!(req.body.login && req.body.password)) {
+    return res.status(400).send({ error: "Data not formatted properly" });
+    }
+    let seconTry = await User.findOne({ login: req.body.login});
+    if (seconTry){
+        res.status(400).send({ error: "User has been verify resently" });
+    }
+    // creating a new mongoose doc from user data
+    const user = new User(req.body);
+    // generate salt to hash password
+    const salt = await bcrypt.genSalt(10);
+    // now we set user password to hashed password
+    user.password = await bcrypt.hash(user.password, salt);
+    user.save().then((doc) => res.status(201).send(doc).catch((err) => 
+    res.status(500).send({ error: err.message })));
 })
 
 app.post("/login", async (req, res) => {
-    connection
-    try {
-        res.status(200).json({ login: req.body.login , password: req.body.password })
-    } catch (error) {
-        console.log(error);
+    const user = await User.findOne({ login: req.body.login });
+    if (user) {
+      const validPassword = await bcrypt.compare(req.body.password, user.password);
+      if (validPassword) {
+        const token = generateAccessToken({ username: req.body.login });
+        res.status(200).json({ message: "Valid password", token: token });
+      } else {
+        res.status(400).json({ error: "Invalid Password" });
+      }
+    } else {
+      res.status(401).json({ error: "User does not exist" });
     }
-
 })
 
 app.get("getData", async (req, res) => {
