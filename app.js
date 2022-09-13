ede8791631d4ee5a3fd7b76fb9f93f63ee70d42a
Karim90403 <karim90403@gmail.com>
2022-09-13 16:18:19 +0300

    testing

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const express = require('express');
 const cors = require('cors');
 const api = require('./api');
 const path = require('path');
-const environment = require('dotenv').config()
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+// app.use(express.static(path.join(__dirname, "dist")))
+app.get("/hello", res => {
+    res.json({success: "helo"})
+})
 
 app.use("/api", api)
-app.use(express.static(path.join(__dirname, "dist")))
 
 
 app.listen(process.env.PORT, () => {
